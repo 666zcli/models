ae12a9622b492f1fec89c09a2ac1ac6cf880030f
Zichao <33247399+666zcli@users.noreply.github.com>
2018-08-28 16:50:27 -0400

    Create seeweights.py

---
+++ tutorials/image/cifar10/seeweights.py
@@ -0,0 +1,10 @@
+#see the results of quantization
+import tensorflow as tf
+import numpy as np
+
+reader = tf.train.NewCheckpointReader('/home/zl198/github/users/666zcli/models/tutorials/image/cifar10/Adam_finetune_bias_tuning_lr_0.0001_ti_150000_ellipse_weight_decay_0.015/cifar10_train/')
+all_variables = reader.get_variable_to_shape_map()
+w1 = reader.get_tensor("conv1/weights")
+print(type(w1))
+print(w1.shape)
+print(w1[0])
