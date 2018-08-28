b941b49f211aa4e869aeebf7c541afb6deb06872
Zichao <33247399+666zcli@users.noreply.github.com>
2018-08-28 16:52:09 -0400

    Update seeweights.py

--- tutorials/image/cifar10/seeweights.py
+++ tutorials/image/cifar10/seeweights.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 import numpy as np
 
-reader = tf.train.NewCheckpointReader('/home/zl198/github/users/666zcli/models/tutorials/image/cifar10/Adam_finetune_bias_tuning_lr_0.0001_ti_150000_ellipse_weight_decay_0.015/cifar10_train/')
+reader = tf.train.NewCheckpointReader('/home/zl198/github/users/666zcli/models/tutorials/image/cifar10/Adam_finetune_bias_tuning_lr_0.0001_ti_150000_ellipse_weight_decay_0.015/cifar10_train/model.ckpt-150000')
 all_variables = reader.get_variable_to_shape_map()
 w1 = reader.get_tensor("conv1/weights")
 print(type(w1))
